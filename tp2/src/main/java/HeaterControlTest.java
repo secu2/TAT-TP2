ba6373eb7e9fa704f283e8d3d744d3f3c5831147
secu2 <clement.secu2@free.fr>
2017-03-07 10:07:51 +0100

    Tests TDD HeaterControl

--- tp2/src/main/java/HeaterControlTest.java
+++ tp2/src/main/java/HeaterControlTest.java
@@ -3,23 +3,45 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Mockito.*;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import com.pholser.junit.quickcheck.When;
 
 
 @RunWith(MockitoJUnitRunner.class)
 public class HeaterControlTest {
 	@Test
 	public void ca_chauffe_sil_fait_froid() {
-       // XXX finish me!
-       assertEquals(hc.getOn(),true);
-       System.out.printf("ca_chauffe_sil_fait_froid: ok.\n");
+		Sensor sensor1 = Mockito.mock(Sensor.class);
+		Sensor sensor2 = Mockito.mock(Sensor.class);
+		Sensor sensor3 = Mockito.mock(Sensor.class);
+
+		Mockito.when(sensor1.getT()).thenReturn((double) 15);
+		Mockito.when(sensor2.getT()).thenReturn((double) 17);
+		Mockito.when(sensor3.getT()).thenReturn((double) 21);
+
+		HeaterControl hc = new HeaterControl(18, sensor1, sensor2, sensor3);
+
+		assertEquals(hc.getOn(),true);
+		System.out.printf("ca_chauffe_sil_fait_froid: ok.\n");
 	}
 	@Test
 	public void ca_chauffe_pas_sil_fait_chaud() {
-       // XXX finish me!
-       assertEquals(hc.getOn(),false);
-       System.out.printf("ca_chauffe_pas_sil_fait_chaud:  ok.\n");
+		Sensor sensor1 = Mockito.mock(Sensor.class);
+		Sensor sensor2 = Mockito.mock(Sensor.class);
+		Sensor sensor3 = Mockito.mock(Sensor.class);
+
+		Mockito.when(sensor1.getT()).thenReturn((double) 18);
+		Mockito.when(sensor2.getT()).thenReturn((double) 17);
+		Mockito.when(sensor3.getT()).thenReturn((double) 24);
+
+		HeaterControl hc = new HeaterControl(18, sensor1, sensor2, sensor3);
+
+		assertEquals(hc.getOn(),false);
+		System.out.printf("ca_chauffe_pas_sil_fait_chaud:  ok.\n");
 	}
 }
 
